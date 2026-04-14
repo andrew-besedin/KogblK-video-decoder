2091b31da4a2ca3f1272c6b5e5052fa94a25f215
Andrew Besedin <andrewprog97@gmail.com>
2026-04-14 03:06:14 +0300

    add: add `test_dll.asm` source file

---
+++ test_dll.asm
@@ -0,0 +1,30 @@
+format PE DLL
+entry DllEntryPoint
+
+include 'win32a.inc'
+
+section '.text' code readable executable
+
+DllEntryPoint:
+	mov eax, 1
+	ret
+
+SampleFunction:
+	mov eax, 123
+	ret
+
+GetSampleValueAddress:
+	mov eax, sample_value
+	ret
+
+section '.data' data readable writeable
+
+sample_value dd 123
+
+section '.edata' export data readable
+
+export 'test_dll.dll', \
+  SampleFunction, 'SampleFunction', \
+  GetSampleValueAddress, 'GetSampleValueAddress'
+
+section '.reloc' fixups data readable discardable
