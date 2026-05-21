04e85781e6164d410950e1d89ae4230a616916d4
Andrew Besedin <andrewprog97@gmail.com>
2026-05-22 02:26:17 +0300

    update: add helloworld running `DllRegisterServer`

--- helloworld.asm
+++ helloworld.asm
@@ -1,33 +1,45 @@
 format PE console
 entry start
 
-include 'win32a.inc'
+include 'win32wxp.inc'
 
 section '.text' code readable executable
 
 start:
 	invoke GetStdHandle, STD_OUTPUT_HANDLE
 	mov [stdout_handle], eax
-	invoke WriteFile, [stdout_handle], message, message_len, bytes_written, 0
+
+	invoke DllRegisterServer
+	invoke wsprintfW, message, format_str, eax
+	lea eax, [eax * 2 + 2]
+	mov [message_len], eax
+
+	invoke WriteFile, [stdout_handle], message, [message_len], bytes_written, 0
 	invoke ExitProcess, 0
 
 section '.data_r' data readable
 
-message du 'Hello, World!', 13, 10
-message_len = $ - message
+format_str du '%08X', 13, 10
 
 section '.data_rw' data readable writeable
 
 stdout_handle dd ?
 bytes_written dd ?
 
+message du 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa', 0
+message_len dd 0
+
 section '.idata' import data readable writeable
 
-library kernel32, 'KERNEL32.DLL'
+library kernel32, 'KERNEL32.DLL', \
+	user32, 'USER32.DLL', \
+	testcomdll, './test_com_dll.dll'
 
-import kernel32, \
-	ExitProcess, 'ExitProcess', \
-	GetStdHandle, 'GetStdHandle', \
-	WriteFile, 'WriteFile'
+include 'api/kernel32.inc'
+include 'api/user32.inc'
+
+import testcomdll, \
+	DllRegisterServer, 'DllRegisterServer', \
+	DllUnregisterServer, 'DllUnregisterServer' 
 
 section '.reloc' fixups data readable discardable
